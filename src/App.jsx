dcc35364dd17eabcae43d44df5953ea4b3fa79bb
fkingBug <fkingbug@gmail.com>
2021-10-19 21:27:00 +0300

    country

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { Card } from './components/Card'
+
 import { Controls } from './components/Controls'
 import { Header } from './components/Header'
+import { List } from './components/List'
 import { Main } from './components/Main'
+import { ALL_COUNTRIES } from './config'
 
 function App() {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data))
+  }, [])
+
   return (
     <>
       <Header />
       <Main>
         <Controls />
+        <List>
+          {countries.map((c) => {
+            const countryInfo = {
+              img: c.flags.png,
+              name: c.name,
+              info: [
+                {
+                  title: 'Population',
+                  description: c.population.toLocaleString(),
+                },
+                {
+                  title: 'Region',
+                  description: c.region.toLocaleString(),
+                },
+                {
+                  title: 'Capital',
+                  description: c.capital.toLocaleString(),
+                },
+              ],
+            }
+            return <Card key={c.name} {...countryInfo} />
+          })}
+        </List>
       </Main>
     </>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,30 @@
 import styled from 'styled-components'
 
+const Wrapper = styled.article``
+
+const CardImage = styled.img``
+
 const CardBody = styled.div``
 
 const CardTitle = styled.h3``
 
+const CardList = styled.ul``
+
 const CardListItem = styled.li``
-export const Card = ({ img, name, info, onClick }) => {
+
+export const Card = ({ img, name, info = [], onClick }) => {
   return (
-    <Wrapper>
+    <Wrapper onClick={onClick}>
       <CardImage />
       <CardBody></CardBody>
+      <CardTitle>{name}</CardTitle>
+      <CardList>
+        {info.map((el) => (
+          <CardListItem key={el.title}>
+            <b>{el.title} : </b> {el.description}
+          </CardListItem>
+        ))}
+      </CardList>
     </Wrapper>
   )
 }
